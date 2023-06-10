7e4ca1b0918d062a200c3882b8ad5ee56f1f82ef
Sandeep Choudhury <sandy271111@outlook.com>
2023-06-10 19:11:12 +0800

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,55 @@
+import Link from "next/link"
+import { signIn, signOut } from 'next-auth/react'
+function Navbar() {
+   
+    return (
+      <nav className='header'>
+        <h1 className='logo'>
+          <a href='#'>NextAuth</a>
+        </h1>
+        <ul className={`main-nav`}>
+          <li>
+            <Link href='/'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href='/dashboard'>
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link href='/blog'>
+              Blog
+            </Link>
+          </li>
+  
+         
+            <li>
+              <Link href='/api/auth/signin' onClick={ e=>{
+                e.preventDefault();
+                signIn('github')
+              }}>
+                
+                Sign In
+              </Link>
+            </li>
+       
+         
+            <li>
+              <Link href='/api/auth/signout' onClick={ e=>{
+                e.preventDefault();
+                signOut()
+              }}>
+               
+                  Sign Out
+                
+              </Link>
+            </li>
+          
+        </ul>
+      </nav>
+    )
+  }
+  
+  export default Navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import NavBar from '@/components/Navbar'
 import '@/styles/globals.css'
+import '@/components/Navbar.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <NavBar/>
+    <Component {...pageProps} />
+    </>
+
+  )
 }
